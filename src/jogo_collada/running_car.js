9445fdf2605ca7037b8bfb2ce1a875bb7cdec3d8
Cleiton Piccini <cleitonPiccini@users.noreply.github.com>
2019-12-10 19:17:46 -0300

    Update running_car.js

--- src/jogo_collada/running_car.js
+++ src/jogo_collada/running_car.js
@@ -5,7 +5,6 @@ import * as THREE from '../../libs/build/three.module.js';
 
 import Stats from '../../libs/examples/jsm/libs/stats.module.js';
 
-import { GUI } from '../../libs/examples/jsm/libs/dat.gui.module.js';
 import { ColladaLoader } from '../../libs/examples/jsm/loaders/ColladaLoader.js';
 import { OrbitControls } from '../../libs/examples/jsm/controls/OrbitControls.js';
 
@@ -21,8 +20,8 @@ import { OrbitControls } from '../../libs/examples/jsm/controls/OrbitControls.js
 //import { OrbitControls } from './jsm/controls/OrbitControls.js';
 
 
-var container, stats, clock, controls;
-var camera, scene, renderer, mixer, avatar;
+var container, stats, controls;
+var camera, scene, renderer, avatar;
 
 var ground;
 			
@@ -39,32 +38,23 @@ var speed_max = -0.6;
 var speed_normal = -0.8;
 var speed_min = -0.3;
 var crash = 0;
+
 init();
 animate();
 
 document.addEventListener("keydown", teclado);
-
-var gui, playbackConfig = {
-    speed: 1.0,
-    wireframe: false
-};
 
 function init() {
 
 	container = document.getElementById( 'container' );
 
 	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 4000 );
-	//camera.position.set( 0, 150, 1300 );
-				
-
-	//camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.set( 15, 10, - 15 );
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color( 0x0000 );
+	scene.background = new THREE.Color( 0xffffff );
+	//scene.background = new THREE.TextureLoader().load( "../../libs/examples/models/collada/uffs.png" );
 	scene.fog = new THREE.Fog( 0xffffff, 1000, 4000 );
-
-	clock = new THREE.Clock();
 
 	var gt = new THREE.TextureLoader().load( "../../libs/examples/models/collada/pista_3.png" );
 	var gg = new THREE.PlaneBufferGeometry( 300, 160000 );
@@ -81,8 +71,8 @@ function init() {
 	// note that because the ground does not cast a shadow, .castShadow is left false
 	ground.receiveShadow = true;
 	scene.add( ground );
+	
 	// collada
-
 	var loader = new ColladaLoader();
 	loader.load( '../../libs/examples/models/collada/mazda_2/mazda.dae', function ( collada ) {
 
@@ -98,8 +88,6 @@ function init() {
 			}
 
 		} );
-		//avatar.multiplyScalar(1.2);
-		//avatar.scale = escala;
 		scene.add( avatar );
 
 	} );
@@ -120,73 +108,11 @@ function init() {
 	light.shadow.camera.top = 350;
 	light.shadow.camera.bottom = - 350;
 	scene.add( light );
-
-	/*var ambientLight = new THREE.AmbientLight( 0xffffff, 0.2 );
-	scene.add( ambientLight );
-
-	var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-	scene.add( camera );
-	camera.add( pointLight );*/
-
-	//
-
+	
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	container.appendChild( renderer.domElement );
-
-	//
-
-	/*   // GUI
-
-	   gui = new GUI();
-
-	   gui.add(playbackConfig, 'speed', 0, 2).onChange(function () {
-   
-		   character.setPlaybackRate(playbackConfig.speed);
-   
-	   });
-   
-	   gui.add(playbackConfig, 'wireframe', false).onChange(function () {
-   
-		   character.setWireframe(playbackConfig.wireframe);
-   
-	   });
-
-	   var config = {
-
-        baseUrl: "../../libs/examples/models/md2/ratamahatta/",
-
-        body: "ratamahatta.md2",
-        skins: [
-            "ratamahatta.png", "ctf_b.png", "ctf_r.png", "dead.png", "gearwhore.png", "coiso.png","ogrobase.png",
-            "pikachu.png","weapon.jpg","Mat_cuerpo_AO.jpeg",
-        ],
-        weapons: [
-            ["weapon.md2", "weapon.png"],
-            ["w_bfg.md2", "w_bfg.png"],
-            ["w_blaster.md2", "w_blaster.png"],
-            ["w_chaingun.md2", "w_chaingun.png"],
-            ["w_glauncher.md2", "w_glauncher.png"],
-            ["w_hyperblaster.md2", "w_hyperblaster.png"],
-            ["w_machinegun.md2", "w_machinegun.png"],
-            ["w_railgun.md2", "w_railgun.png"],
-            ["w_rlauncher.md2", "w_rlauncher.png"],
-            ["w_shotgun.md2", "w_shotgun.png"],
-            ["w_sshotgun.md2", "w_sshotgun.png"]
-        ]
-	};
-	
-	character.onLoadComplete = function () {
-
-        setupSkinsGUI(character);
-        setupWeaponsGUI(character);
-        setupGUIAnimations(character);
-
-        character.setAnimation(character.meshBody.geometry.animations[0].name);
-
-    };*/
-   
 
 	controls = new OrbitControls( camera, renderer.domElement );
 	controls.target.set( 0, 2, 0 );
