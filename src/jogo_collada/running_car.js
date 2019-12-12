45fa85b029ce6a5929fdef9538c49e6a75eae55f
cleitonPiccini <cleiton-piccini@hotmail.com>
2019-12-11 23:41:09 -0300

    Versão final possuia erros, erros corrigidos, porém repositório ainda não funciona no gitHub Pages

--- src/jogo_collada/running_car.js
+++ src/jogo_collada/running_car.js
@@ -21,8 +21,8 @@ var ground;
 var pedras;
 var tam_obstaculo = 2.5, pos_init_z_obstaculo = 40, pos_init_x_obstaculo = 5;
 var crash = 0, pedra_posit = pos_init_x_obstaculo, eixo_z_virtual = 40;
-var max_rand_obstaculo = 13.5; // Tamanho base para gerar os números randomicos para o eixo x do obstaculo.
-var timeObstaculo = 3000; // Tempo para surgir um novo obstaculo.
+var max_rand_obstaculo = 13.0; // Tamanho base para gerar os números randomicos para o eixo x do obstaculo.
+var timeObstaculo = 4000; // Tempo para surgir um novo obstaculo.
 
 //Controle movimentos carro
 var pos_value = 0.0; //Posição do eixo x do carro. (mesma função do eixo_z_virtual).
@@ -31,7 +31,7 @@ var limite_l = 5.25; //Limite para velocidade plena do lado esquerdo da pista
 var l_crash = 7.0; //Limite para do lado esquerdo da pista
 var r_crash = -6.0; //Limite para do lado direito da pista
 var speed_max = -0.6;
-var speed_normal = 1.0;
+var speed_normal = 1.2;
 var speed_min = 0.3;
 
 
@@ -169,12 +169,11 @@ function animate() {
 
 function geraObstaculo(){
 
-	if (crash == 0){
-		var loop = setTimeout(function(){ 
-	  		geraObstaculo();
-			}, timeObstaculo);
-		obstaculosRand(max_rand_obstaculo);
-	}
+	var loop = setTimeout(function(){ 
+		geraObstaculo();
+		}, timeObstaculo);
+	obstaculosRand(max_rand_obstaculo);
+
 }
 
 function render() {
